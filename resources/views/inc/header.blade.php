727c6f0998dee212bb4825fda9dc214658d160d5
rodrigo <mcd77.1990@gmail.com>
2019-06-25 18:13:56 -0300

    Initial commit

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,52 @@
+	<header id="menu-center" >
+			<div  class="row">
+				<div class="container-fluid">
+					<div class="col-lg-2 hidden-md hidden-sm hidden-xs ">
+						<a href="index" id="logo-btn" style="cursor:pointer">
+							<img class="logo" src="<?php echo asset("img/logo-blue.svg")?>"></img> 
+
+						</a>
+					</div>
+
+					<div class="col-lg-10 hidden-md hidden-sm hidden-xs btn-cont">
+						<ul id="nav" class="menu">
+							<li><a href=""  class="after-btn">PROYECTOS</a></li>
+							<li><a href="" target="" class="after-btn"  >NOSOTROS</a></li>
+							<li><a href="" class="after-btn">COLABORÁ</a></li>
+
+							<li class="">
+								
+								<a href="https://www.facebook.com/argenpesos" target="_blank" class="float-left rr-ss"><i class="fab fa-facebook-f"></i></a>
+
+
+								<a href="https://www.instagram.com/argenpesos/" target="_blank" class="float-left rr-ss"><i class="fab fa-instagram"></i></a>
+							</li>
+							
+						</ul>
+					</div>
+
+				</div>
+				<div class="hidden-lg col-md-12 col-sm-12 col-xs-12 text-center xs-row " style="">
+					<a href="index.php">
+						<img src="<?php echo asset("img/logo-blue.svg")?>" width="150px"></img> 
+					</a>
+						<a id="abrirMenu">
+							<i class="fa fa-bars"  id="abrirMenu"></i>
+						</a>
+					</div>
+			</div>
+		</header>
+		<ul id="xsMenu" class="overlay-xs-menu menu">
+			<div class="row">
+				<a id="cerrarMenu"  class="close-menu-xs">
+					<i class="fas fa-times"></i>
+				</a>
+			</div>
+			<li><a href="saca_tu_prestamo" class="xs-btn spa-btn">SACA TU PRÉSTAMO</a></li>
+			<li><a href="https://www.argenpesos.com.ar/micuenta/quiero-pagar.php"  class="xs-btn" >CONSULTÁ TU CUENTA</a></li>
+			<li><a href="sucursales" class="xs-btn spa-btn">NUESTRAS SUCURSALES</a></li>
+
+			<li><a href="institucional" class="xs-btn spa-btn">INSTITUCIONAL</a></li>
+
+			<li><a href="resp_social" class="xs-btn spa-btn">RESPONSABILIDAD SOCIAL</a></li>
+		</ul>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@include('inc.header')
+
+<div class="bkground">
+	<h1><b>M</b>ovimiento de <b>A</b>ccion <b>S</b>ocial</h1>
+
+	<ul class="flex">
+		<li>
+			<h1>¿QUERÉS <br> COLABORAR?</h1>
+		</li>
+		<li><h2><span>ARS</span> <br> $200</h2></li>
+		<li><h2><span>ARS</span> <br> $300</h2></li>
+		<li><h2><span>ARS</span> <br> $500</h2></li>
+		<li><h2><span>OTRO</span> <br> MONTO</h2></li>
+	</ul>
+</div>
